c2c1ee51f7c31ff53afe179a9911294ce1dea512
Mayank Dawar <mayankdawar29@gmail.com>
2020-05-02 00:15:29 +0530

    Create Dial.java

---
+++ app/src/main/java/bsafe/bsafe/Dial.java
@@ -0,0 +1,25 @@
+package bsafe.bsafe;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Dial extends AppCompatActivity  {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
