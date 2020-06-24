5cea2a12b3d280e6c7a9279e0797fe27f1da2bc3
Mayank Dawar <mayankdawar29@gmail.com>
2020-06-24 11:56:53 +0530

    Update MainActivity.java

--- app/src/main/java/bsafe/bsafe/MainActivity.java
+++ app/src/main/java/bsafe/bsafe/MainActivity.java
@@ -8,9 +8,14 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -18,6 +23,7 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.provider.MediaStore;
 import android.telephony.SmsManager;
 import android.util.Log;
@@ -25,8 +31,26 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Collections;
+
 public class MainActivity extends AppCompatActivity {
-    ImageButton amb,pol,fam,hosp,nearPol,emergency,pro;
+    ImageButton amb,pol,fam,hosp,nearPol,emergency,pro,mainLogo;
     Intent intent;
     LocationManager locationManager;
     LocationListener locationListener;
@@ -34,7 +58,14 @@ public class MainActivity extends AppCompatActivity {
     int nearCallId;
     MediaPlayer music;
     private static int Video_req = 101;
+    private LocationSettingsRequest.Builder builder;
     private Uri videoUri = null;
+    private final int REQUEST_CHECK_CODE = 0;
+    private SensorManager sm;
+    private  float acelVal, acelLast;
+    private static float shake;
+    public static int countShake = 0;
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -56,6 +87,69 @@ public class MainActivity extends AppCompatActivity {
         nearPol();
         siren();
         editDetails();
+        Camera();
+        turnOnGps();
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sm.registerListener(sensorListner, sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        acelVal = SensorManager.GRAVITY_EARTH;
+        acelVal = SensorManager.GRAVITY_EARTH;
+        shake = 0.0f;
+    }
+    public void turnOnGps(){
+        LocationRequest request = new LocationRequest()
+                .setFastestInterval(1500)
+                .setInterval(3000)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        builder = new LocationSettingsRequest.Builder()
+                .addAllLocationRequests(Collections.singleton(request));
+
+        Task<LocationSettingsResponse> result =
+                LocationServices.getSettingsClient(this).checkLocationSettings(builder.build());
+
+        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+                try {
+                    task.getResult(ApiException.class);
+                } catch (ApiException e) {
+                    switch (e.getStatusCode()){
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            try {
+                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                resolvableApiException.startResolutionForResult(MainActivity.this,REQUEST_CHECK_CODE);
+                            } catch (IntentSender.SendIntentException ex) {
+                                ex.printStackTrace();
+                            }catch (ClassCastException ex){
+
+                            }
+                            break;
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        {
+                            break;
+                        }
+
+                    }
+                }
+            }
+        });
+
+    }
+
+    public void Camera(){
+        emergency = findViewById(R.id.emergency);
+        emergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                if(videoIntent.resolveActivity(getPackageManager())!=null)
+                {
+                    startActivityForResult(videoIntent,Video_req);
+                }
+                Send();
+            }
+        });
+
     }
 
 
@@ -96,17 +190,22 @@ public class MainActivity extends AppCompatActivity {
             locationListener = new LocationListener() {
                     @Override
                     public void onLocationChanged (Location location){
-                        currentLat = location.getLatitude();
-                        currentLong = location.getLongitude();
-                        Log.i("Location", location.toString());
-                        String message = "https://www.google.com/maps/@"+currentLat+","+currentLong+","+"15z";
-                        String Message = "Your friend abc is in problem , Here's the Location\nLatitude :"+currentLat +"\nLongitude : " + currentLong+"\n"+message;
-                        String numbers[] = fetch();
-                        SmsManager mySmsManager = SmsManager.getDefault();
-                        for(String number : numbers) {
-                            mySmsManager.sendTextMessage(number, null, Message, null, null);
-                            Toast.makeText(getApplicationContext(), Message, Toast.LENGTH_SHORT).show();
+                        try{
+                            currentLat = location.getLatitude();
+                            currentLong = location.getLongitude();
+                            Log.i("Location", location.toString());
+                            String message = "https://www.google.com/maps/@"+currentLat+","+currentLong+","+"15z";
+                            String Message = "Your friend abc is in problem , Here's the Location\nLatitude :"+currentLat +"\nLongitude : " + currentLong+"\n"+message;
+                            String numbers[] = fetch();
+                            SmsManager mySmsManager = SmsManager.getDefault();
+                            for(String number : numbers) {
+                                mySmsManager.sendTextMessage(number, null, Message, null, null);
+                                Toast.makeText(getApplicationContext(), Message, Toast.LENGTH_SHORT).show();
+                            }
+                        }catch (Exception e){
+                            e.printStackTrace();
                         }
+
                 }
                     @Override
                     public void onStatusChanged (String s,int i, Bundle bundle){
@@ -142,22 +241,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
     public void siren()
     {
         music = MediaPlayer.create(this, R.raw.policesiren);
-        emergency = findViewById(R.id.emergency);
-        final int i = 1;
-        emergency.setOnClickListener(new View.OnClickListener() {
+        mainLogo = findViewById(R.id.logo);
+        mainLogo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                intent = new Intent(getApplicationContext(),CameraMain.class);
-                startActivity(intent);
-                //music.setLooping(true);
-                //music.start();
-                Send();
+                music.setLooping(true);
+                music.start();
+                //Send();
             }
         });
-
     }
     public void nearPol()
     {
@@ -260,5 +356,54 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Video_req && resultCode == RESULT_OK) {
+            videoUri = data.getData();
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        countShake = 0;
+    }
+
+    private final SensorEventListener sensorListner = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            acelLast = acelVal;
+            int i=5;
+            acelVal = (float) Math.sqrt((double) (x*x + y*y + z*z));
+            float delta = acelVal - acelLast;
+            shake = shake * 0.9f + delta;
+            if (shake>30){
+                shake = 0;
+                countShake = countShake + 1;
+                siren();
+                if(countShake >= 3){
+                    Send();
+                    vibration();
+                    Toast toast = Toast.makeText(getApplicationContext(), "Do not shake me", Toast.LENGTH_SHORT);
+                    toast.show();
+                    shake = 30;
+                }
+            }
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+    public void vibration(){
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        music = MediaPlayer.create(this, R.raw.policesiren);
+        music.setLooping(true);
+        music.start();
+        vibrator.vibrate(5000);
     }
 }
